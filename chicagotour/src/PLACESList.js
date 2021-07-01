aa84ca25f45c0cd1423e31d95c2635b4d694a8cb
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-30 23:23:36 -0500

    updating PLaces list component

--- chicagotour/src/PLACESList.js
+++ chicagotour/src/PLACESList.js
@@ -3,11 +3,7 @@ import logo from './logo.svg';
 
 function PLACESList() {
     return (
-        <div>
-            <img src={logo} width="300" height="300" alt="Hello" />
-            <h1>Name...</h1>
-            <h1>Address...</h1>
-        </div>
+       <h1>Keep your eye on this place for future content...</h1>
     )
 }
 
