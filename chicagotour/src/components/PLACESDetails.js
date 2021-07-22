52001ea33842d93e1aa0bb9088849f974ef9d31e
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-21 23:44:18 -0500

    updating prjct structure

---
+++ chicagotour/src/components/PLACESDetails.js
@@ -0,0 +1,24 @@
+import data from '../places-data.json';
+import React from 'react'
+
+export default function PLACESDetails(props) {
+    const { id } = props.match.params;
+    const { images, title, desc, hours, features, geo } = data[id];
+
+    return (
+        <div>
+            <div>
+                <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt="Hello"/>
+            </div>
+
+            <div>
+                <h1>{title}</h1>
+                <p>{desc}</p>
+                <p>{hours}</p>
+                <p>{features}</p>
+                <p>{geo.lat} {geo.lon}</p>
+            </div>
+
+        </div>
+    )
+}
