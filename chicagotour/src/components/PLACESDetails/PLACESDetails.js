327fe1a72944e7afc942e2b3828052ee3d769abb
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-23 22:32:01 -0500

    adding styling to details page

--- chicagotour/src/components/PLACESDetails/PLACESDetails.js
+++ chicagotour/src/components/PLACESDetails/PLACESDetails.js
@@ -1,22 +1,23 @@
 import data from '../../places-data.json';
-import React from 'react'
+import React from 'react';
+import './PLACESDetails.css';
 
 export default function PLACESDetails(props) {
     const { id } = props.match.params;
     const { images, title, desc, hours, features, geo } = data[id];
 
     return (
-        <div>
-            <div>
+        <div className="PLACESDetails">
+            <div className="PLACESDetails-image">
                 <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt="Hello"/>
             </div>
 
-            <div>
-                <h1>{title}</h1>
-                <p>{desc}</p>
-                <p>{hours}</p>
-                <p>{features}</p>
-                <p>{geo.lat} {geo.lon}</p>
+            <div className="PLACESDetails-info">
+                <h1 className="PLACESDetails-title">{title}</h1>
+                <p className="PLACESDetails-desc">{desc}</p>
+                <p className="PLACESDetails-hours">{hours}</p>
+                <p className="PLACESDetails-features">{features}</p>
+                <p className="PLACESDetails-geo">{geo.lat} {geo.lon}</p>
             </div>
 
         </div>
