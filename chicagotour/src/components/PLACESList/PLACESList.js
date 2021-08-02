621d7bb7f870bdf484a4b7fc93f5532b18344077
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-01 21:59:22 -0500

    adding search functionality

--- chicagotour/src/components/PLACESList/PLACESList.js
+++ chicagotour/src/components/PLACESList/PLACESList.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PLACESSpace from '../PLACESSpace/PLACESSpace';
 import './PLACESList.css';
 import data from '../../places-data.json';
 
 export default function PLACESList() {
 
+    const [query, setQuery] = useState('');
+
     const places = data.map(({ title, address, images, hours }, i) => {
-        
+
         return (
             <PLACESSpace
-                id = {i}
+                id={i}
                 key={title}
                 name={title}
                 address={address}
@@ -21,6 +23,14 @@ export default function PLACESList() {
 
     return (
         <div className="PLACESList">
+            <form>
+                <input
+                    value={query}
+                    placeholder='search'
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button type='submit'>Submit</button>
+            </form>
             {places}
         </div>
     )
